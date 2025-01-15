d8e33b21f46065a2d611eac302363517ba24be42
ChriSanabra <sanabrachris@gmail.com>
2025-01-15 09:26:31 -0600

    COMMIT 2

--- poo_chris/src/main/java/edu/chris/sanabra/actividades/act1/Starter.java
+++ poo_chris/src/main/java/edu/chris/sanabra/actividades/act1/Starter.java
@@ -3,7 +3,11 @@ package edu.chris.sanabra.actividades.act1;
 import edu.chris.sanabra.actividades.act1.ui.CLI;
 public class Starter {
     public static void main(String[] args) {
+      
         CLI.ShowMenu();
-    }
+        CLI.ShowMessage(null);
+        CLI.ShowError(null);
+        CLI.ShowCarrito(null, null, null, null);
 
+    }
 }
--- poo_chris/src/main/java/edu/chris/sanabra/actividades/act1/ui/CLI.java
+++ poo_chris/src/main/java/edu/chris/sanabra/actividades/act1/ui/CLI.java
@@ -2,9 +2,24 @@ package edu.chris.sanabra.actividades.act1.ui;
 
 public class CLI {
     public static void ShowMenu() {
-        System.out.println("Welcome to Chris app");
-        System.out.println("1. Muestra carrito");
-        System.out.println("2. Agrega run run run");
-        
+        System.out.println("Sea bienvenido al consesionario run run");
+        System.out.println("1. Agrega un carrito");
+        System.out.println("2. Mostrar fecha del coche"); 
+        System.out.println("3. Salir"); 
+    }
+
+    public static void ShowMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static void ShowError(String message) {
+        System.out.println("Error: " + message);
+    }
+
+    public static void ShowCarrito(String marca, String modelo, String color, String fecha) {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Color: " + color);
+        System.out.println("Fecha: " + fecha);
     }
 }
